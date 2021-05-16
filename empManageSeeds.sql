37878466263f671a6812cebef4c9686b0fb0a6da
Mandell Butler <mandell.butler@outlook.com>
2021-05-16 11:45:26 -0400

    Restructured seed.sql

--- empManageSeeds.sql
+++ empManageSeeds.sql
@@ -11,13 +11,10 @@ CREATE TABLE department (
 );
 
 INSERT INTO department (id, dept_name)
-VALUES (100, "Front of House");
-
-INSERT INTO department (id, dept_name)
-VALUES (200, "Back of House");
-
-INSERT INTO department (id, dept_name)
-VALUES (300, "Admin");
+VALUES 
+(100, "Front of House"),
+(200, "Back of House"),
+(300, "Admin");
 
 
 CREATE TABLE role (
@@ -28,25 +25,14 @@ CREATE TABLE role (
 );
 
 INSERT INTO role (id, title, salary, department_id)
-VALUES (11, "Server", 40000, 100);
-
-INSERT INTO role (id, title, salary, department_id)
-VALUES (12, "Bartender", 60000, 100);
-
-INSERT INTO role (id, title, salary, department_id)
-VALUES (13, "Host", 26000, 100);
-
-INSERT INTO role (id, title, salary, department_id)
-VALUES (21, "Line Cook", 24000, 200);
-
-INSERT INTO role (id, title, salary, department_id)
-VALUES (22, "Dish Washer", 24000, 200);
-
-INSERT INTO role (id, title, salary, department_id)
-VALUES (31, "Manager", 83000, 300);
-
-INSERT INTO role (id, title, salary, department_id)
-VALUES (32, "General Manager", 100000, 300);
+VALUES
+(11, "Server", 40000, 100),
+(12, "Bartender", 60000, 100),
+(13, "Host", 26000, 100),
+(21, "Line Cook", 24000, 200),
+(22, "Dish Washer", 24000, 200),
+(31, "Manager", 83000, 300),
+(32, "General Manager", 100000, 300);
 
 
 CREATE TABLE employee (
@@ -59,88 +45,35 @@ CREATE TABLE employee (
 );
 
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES ("Jason", "Weaver", 11, 311);
-
-INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES ("Jose", "Garcia", 22, 320);
-
-INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES ("Carey", "Wayne", 11, 311);
-
-INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES ("Laura", "Mendoza", 21, 320);
-
-INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES ("Janielle", "Thomas", 13, 312 );
-
-INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES ("Philipe", "Cardoza", 21, 320);
-
-INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES ("Josephine", "Cooks", 13, 312);
-
-INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES ("William", "Lewis", 21, 320);
-
-INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES ("Jecobi", "Reece", 12, 312);
-
-INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES ("Philip", "Seams", 21, 320);
-
-INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES ("James", "Diggs", 11, 311);
-
-INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES ("Mary", "Beales", 21, 320);
-
-INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES ("Kevin", "Calloway", 11, 311);
-
-INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES ("Kesley", "Wrangler", 12, 312);
-
-INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES ("Oliver", "James", 22, 320);
-
-INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES ("Jenni", "Meyers", 11, 311);
-
-INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES ("Jason", "Douglas", 13, 312);
-
-INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES ("Anthony", "Piaz", 22, 320);
-
-INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES ("Lisa-Anne", "Gates", 11, 311);
-
-INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES ("Mitchell", "Sands", 11, 311);
-
-INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES ("Stephanie", "Douglas", 11, 311);
-
-INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES ("Tracy", "Calhoun", 11, 311);
-
-INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES ("Keisha", "Holmes", 11, 311);
-
-INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES ("Tanisha", "Davis", 12, 312);
-
-INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES ("Christopher", "Tompson", 12, 312);
-
-INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES ("Jacob", "Lendoff", 12, 312);
-
-INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES ("Weazy", "Jefferson", 21, 320);
-
-INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES ("Daphne", "Greene", 11, 311);
+VALUES 
+("Jason", "Weaver", 11, 311),
+("Jose", "Garcia", 22, 320),
+("Carey", "Wayne", 11, 311),
+("Laura", "Mendoza", 21, 320),
+("Janielle", "Thomas", 13, 312),
+("Philipe", "Cardoza", 21, 320),
+("Josephine", "Cooks", 13, 312),
+("William", "Lewis", 21, 320),
+("Jecobi", "Reece", 12, 312),
+("Philip", "Seams", 21, 320),
+("James", "Diggs", 11, 311),
+("Mary", "Beales", 21, 320),
+("Kevin", "Calloway", 11, 311),
+("Kesley", "Wrangler", 12, 312),
+("Oliver", "James", 22, 320),
+("Jenni", "Meyers", 11, 311),
+("Jason", "Douglas", 13, 312),
+("Anthony", "Piaz", 22, 320),
+("Lisa-Anne", "Gates", 11, 311),
+("Mitchell", "Sands", 11, 311),
+("Stephanie", "Douglas", 11, 311),
+("Tracy", "Calhoun", 11, 311),
+("Keisha", "Holmes", 11, 311),
+("Tanisha", "Davis", 12, 312),
+("Christopher", "Tompson", 12, 312),
+("Jacob", "Lendoff", 12, 312),
+("Weazy", "Jefferson", 21, 320),
+("Daphne", "Greene", 11, 311);
 
 CREATE TABLE manager (
   id INT PRIMARY KEY,
@@ -150,14 +83,9 @@ CREATE TABLE manager (
   manager_id INT NULL
 );
 
-INSERT INTO manager (id, first_name, last_name, role_id)
-VALUES (300, "Jesus", "Cristo", 32);
-
 INSERT INTO manager (id, first_name, last_name, role_id, manager_id)
-VALUES (311, "Jessie", "James", 31, 300);
-
-INSERT INTO manager (id, first_name, last_name, role_id, manager_id)
-VALUES (312, "Jennifer", "London", 31, 300);
-
-INSERT INTO manager (id, first_name, last_name, role_id, manager_id)
-VALUES (320, "Julie", "Grey", 31, 300);
+VALUES 
+(300, "Jesus", "Cristo", 32, NULL),
+(311, "Jessie", "James", 31, 300),
+(312, "Jennifer", "London", 31, 300),
+(320, "Julie", "Grey", 31, 300);
